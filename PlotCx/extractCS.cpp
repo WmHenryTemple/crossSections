bc684c31755830f224a196a2d6e7763e7ea7e5c2
wmhenrytemple <billyh@temple.edu>
2024-08-21 14:00:48 -0400

    reproducing paper results

--- PlotCx/extractCS.cpp
+++ PlotCx/extractCS.cpp
@@ -13,9 +13,8 @@
 // returns cross section in a TGraph for a given spectrometer/kinematic if cs==1 (w2==6)
 // returns ratio xsec/model in a TGraph for a given spectrometer/kinematic if cs==0  (w2==5)
 
-TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21",string mom="5p1", int cs=0, string pass="pass325", string xaxis="w2", double w2rebin=0){
+TGraphErrors* extractCS(string spec="shms", string target="r", string angle="21",string mom="2p7", int cs=1, string pass="pass400", string xaxis="xb", double w2rebin=0){
 
-  
   cs=cs+5;//W2 binning
   cout << "*****************  extracting .... **************************"<<endl;
   cout <<"spec="<<spec<<endl;
@@ -26,9 +25,10 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   cout <<"pass="<<pass<<endl;
   cout <<"xaxis"<<xaxis<<endl;
   w2rebin=doIrebin(spec,angle,mom);
-  cout <<"w2rebin"<<w2rebin<<endl;
+  cout <<"w2rebin: "<<w2rebin<<endl;
   //  w2rebin=0;
-  ///////////////////////?/////////////////////////////////////////////////
+
+  ////////////  need rebin for getKinErrorFromMc ///////////////////////////
   bool rebin=true;
   if(spec=="hms" && angle=="21" && (mom=="5p1" || mom=="5p7"))rebin=false;
   if(spec=="shms" && angle=="21" && (mom=="4p0" || mom=="5p1"))rebin=false;
@@ -36,30 +36,29 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   if(spec=="shms" && angle=="29" && (mom=="3p0" || mom=="3p7"))rebin=false;
   if(spec=="shms" && angle=="33" && (mom=="2p6" || mom=="3p2"))rebin=false;
   if(spec=="shms" && angle=="39" && (mom=="2p0" || mom=="2p5"))rebin=false;
+  rebin=false;//to make livetime error work,
 
-  rebin=false;
+  ///////////////////////?/////////////////////////////////////////////////
   //  ofstream ofile3;
   //  ofile3.open("trash.txt",ios::app | ios::out );
   //  ofile3 << spec <<"\t"<< angle <<"\t"<< mom <<"\t"<< rebin<<endl;
   //  ofile3.close();
   ///////////////////////?/////////////////////////////////////////////////
-
   //  int cnt=0;
   // cout << "Hello"<<cnt<<endl;cnt++;
   //  ofstream ofile;
-  //  ofile.open("q2rangeMthn.txt",ios::app | ios::out );
-
-  ////////////////////////////
+  //   ofile.open("q2rangeMthn.txt",ios::app | ios::out );
+  ///////////////////////?/////////////////////////////////////////////////
 
   TH1F *hkinErr;
-    /*
-=getKinErrorFromMc(target, angle, mom, spec,cs);
-  //  cout << "Get the kin error hostogram"<<endl;
-  if(rebin && cs<5){
-  hkinErr->Rebin(3);
-  hkinErr->Scale(1/3.);
-  }
-    */
+  /*
+    =getKinErrorFromMc(target, angle, mom, spec,cs);
+    //  cout << "Get the kin error hostogram"<<endl;
+    if(rebin && cs<5){
+    hkinErr->Rebin(3);
+    hkinErr->Scale(1/3.);
+    }
+  */
   double ang=21;
   double spec_flag=0.;
   if(spec=="shms")spec_flag=1.;
@@ -67,6 +66,9 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   if(angle=="29")ang=29;
   if(angle=="33")ang=33;
   if(angle=="39")ang=39;
+
+  //////////////////////////////
+  // Charge Error
   //////////////////////////////
   ////   Just for printing charge error
   //  ofstream ofile2;
@@ -78,16 +80,19 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   if(target=="h")charge_err=qh_err;
   if(target=="d")charge_err=qd_err;
   if(target=="r")charge_err=qr_err;
-  
   // add charge error to pt2pt
   //  ofile2 << spec_flag << "\t"<< ang << "\t" << qh_err << "\t"<< qd_err <<endl; 
 
+  //////////////////////////////
+  // Radiative Correction Error
+  //////////////////////////////
   TGraph *g_rad;
   if(angle=="21")g_rad=getRadError(21, target);
   if(angle=="25")g_rad=getRadError(25, target);
   if(angle=="29")g_rad=getRadError(29, target);
   if(angle=="33")g_rad=getRadError(33, target);
   if(angle=="39")g_rad=getRadError(39, target);
+  if(angle=="59")g_rad=getRadError(59, target);
 
   Float_t deltad, ratiod, errd, ep, modeld, binWidth, w2d;
   Float_t deltah, ratioh, errh, modelh,csbh, csbd, w2h;
@@ -110,14 +115,14 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   Double_t ebeam=10.602;
   Double_t hsec=pc*(1+pOffset);
   //  Double_t thetac=getAngle(angle,spec);
-  // extracting cs and nominal shms angle (no beamtheta)
-  Double_t thetac=getAngle(angle,"shms");
+  // extracting cs at nominal shms angle (no beamtheta)
+  Double_t thetac=getAngle(angle,"shms");// so we can plot HMS/SHMS 21 deg
+
 
   string version="v996t2";
   if(pass=="pass310")version="v0.995";
   if(pass=="pass311")version="v0.990";
   cout << "Going to use version "<<version<<" for "<<pass<<endl;
-
   TGraph2D *grh=getRadCorrW2("h",1,spec,version);  
   grh->SetName("grh");
   TGraph2D *grd=getRadCorrW2("d",1,spec,version);  
@@ -138,12 +143,9 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   if(cs==3)hrdd=(TH1F*)frd->Get("hrd_stat");    // stat error only  
   if(cs>=5)hrdd=(TH1F*)frd->Get("hsw2");    // W2
   if(cs==8)hrdd=(TH1F*)frd->Get("hsw2_stat");    // W2
-  //  if(cs==8)hrdd=(TH1F*)frd->Get("hsw2_stat");    // W2
   if(cs>=5){
     hmdd=(TH1F*)frd->Get("hmw2");    // W2
-    if(rebin&&cs<5)hmdd->Rebin(3);
     if(w2rebin!=0){
-
       hmdd->Rebin(w2rebin);
       hrdd->Rebin(w2rebin);
       cout << "In the MC here are "<<hmdd->GetNbinsX()<<endl;
@@ -153,20 +155,19 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
     }
       hrdd->Divide(hmdd);    // W2
   }
+  // livetime and boiling errors
   TH1F *hlte_d=(TH1F*)frd->Get("herr_live");  
   TH1F *hboil_d=(TH1F*)frd->Get("herr_boil");  
   hrdd->SetDirectory(0);
-  //  hmdd->SetDirectory(0);
   hlte_d->SetDirectory(0);
   hboil_d->SetDirectory(0);
-  // correctrun run 1640 for temp. fluctuation
+  // correctrun run 1640 for temp. fluctuation ELOG xxx
   if( (target=="r" || target=="d") && spec=="hms" && angle=="21" && mom == "3p3"){
     hrdd->Scale(1.0229);
     cout << endl << endl <<"Correcting for run 1640" << endl << endl;
   }
   frd->Close();
-  
-
+  //*******************************************************************************************  
   // Hydrogen
   TFile *frh=new TFile(Form("ratiosOut/%s/%s_ratiosh%sdeg%s.root",pass.c_str(),spec.c_str(),angle.c_str(),mom.c_str()));
   TH1F *hrdh, *hmdh;
@@ -174,52 +175,44 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
   if(cs==3)hrdh=(TH1F*)frh->Get("hrd_stat");  // stat error only
   if(cs>=5)hrdh=(TH1F*)frh->Get("hsw2");    // W2
   if(cs==8)hrdh=(TH1F*)frh->Get("hsw2_stat");    // W2
-  //  if(cs==8)hrdh=(TH1F*)frh->Get("hsw2_stat");    // W2
   if(cs>=5){
-    //    cout <<"Hello1"<<endl;
     hmdh=(TH1F*)frh->Get("hmw2");    // W2
-    //    cout <<"Hello2"<<endl;
-    if(rebin&&cs<5)hmdh->Rebin(3);
     if(w2rebin!=0){
       hmdh->Rebin(w2rebin);
       hrdh->Rebin(w2rebin);
-      //      cout <<"Hello3"<<endl;
-      //      cout <<"Hello4"<<endl;
     }
     hrdh->Divide(hmdh);    // W2
-
   }
-
-  //  hrdd->Draw();
-  //  hrdh->Draw("same");
-
-  //  cout << "MC Bin 151 W2="<<hrdh->GetBinCenter(151)<<"  Content="<<hrdh->GetBinContent(151);
+  // livetime and boiling errors
   TH1F *hlte_h=(TH1F*)frh->Get("herr_live");  
   TH1F *hboil_h=(TH1F*)frh->Get("herr_boil");  
   hrdh->SetDirectory(0);
-  //  hmdh->SetDirectory(0);
   hlte_h->SetDirectory(0);
   hboil_h->SetDirectory(0);
   frh->Close();
-
+  //*******************************************************************************************  
   cout << "Got ratio files, about to loop..."<<endl;
   const Int_t nbins=hrdd->GetNbinsX();
+  cout << "There are "<<nbins<<" bins in hrdd"<<endl;
+
   bool otherSide=false;
   double wmin=0;
   double wmax=0;
   bool first=true;
   int leftPts=0;
   int rightPts=0;
-  TGraphErrors *gmodDep;
-  double modErr;
-  //  if(cs==2 || cs==7 ){
+  double modErr=0;
+
+  //  Error for model dependence
   TFile *fff=new TFile("modelDepError.root");
   string gname=spec+"_"+target+angle+"deg";
   cout << "gname: "<< gname<< endl;
-  gmodDep=(TGraphErrors*)fff->Get(gname.c_str())->Clone();
+  TGraphErrors *gmodDep;
+  if(angle!="59")gmodDep=(TGraphErrors*)fff->Get(gname.c_str())->Clone();
 
-  //  }
-
+  //*******************************************************************************************  
+  //                                 MAIN LOOP
+  //*******************************************************************************************  
   //go bin by bin and wt ratios
   for (Int_t i=1; i<=nbins; i++)
     {
@@ -234,36 +227,46 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 
       deltah=hrdh->GetBinCenter(i);
       w2h=hrdd->GetBinCenter(i);
-      binWidth=hrdh->GetBinWidth(i);
       ratioh=hrdh->GetBinContent(i);
       errh=hrdh->GetBinError(i); 
 
+      binWidth=hrdh->GetBinWidth(i);//not used atm
+
+      cout <<endl<< "start bin:" << i << "  w2: "<<deltad<<endl;      
+
+      // Calc E' and delta if W2 binned 
       ep=(1+deltah/100)*hsec;
       Float_t mp = .9382723;
       Double_t sin2 = sin(thetac/2/180*TMath::Pi())*sin(thetac/2/180*TMath::Pi());      
       if(cs>=5){
 	ep=(2*mp*ebeam+mp*mp-deltah)/(2*mp+4*ebeam*sin2);
+	//	cout <<"Eprime: "<<ep<<endl;
 	deltah=(ep/hsec-1.)*100;
 	deltad=(ep/hsec-1.)*100;
       }
-      
+      //      cout << "b4 delta cut, bin:" << i << "  W2: "<<deltad<<endl;      
+
+      // No idea what I was doing here.  Why not a delta check on HMS?
       if( (deltad>-10&&deltad<22&&spec=="shms") || spec=="hms") //This is how I handle rebinned data
 	{
+	  cout << "after delta cut, bin:" << i <<endl;      
+
 	  //Debs delta correction for SHMS  as of data pass 62 its in datayield.cpp 
-	  if(spec=="shms")
-	    {
-	      double p0 = 1.00156;
-	      double p1 = -0.002473; 
-	      double p2 = -1.54588e-05;
-	      double p3 = 6.63986e-06;
-	      double shms_delta_corr=p0+p1*deltad+p2*pow(deltad,2)+p3*pow(deltad,3);
-	      //	      ratiod=ratiod/shms_delta_corr;
-	      //	      ratioh=ratioh/shms_delta_corr;
-	    }
+	  //	  if(spec=="shms")
+	  //	    {
+	  //	      double p0 = 1.00156;
+	  //	      double p1 = -0.002473; 
+	  //	      double p2 = -1.54588e-05;
+	  //	      double p3 = 6.63986e-06;
+	  //	      double shms_delta_corr=p0+p1*deltad+p2*pow(deltad,2)+p3*pow(deltad,3);
+	  //	      ratiod=ratiod/shms_delta_corr;
+	  //	      ratioh=ratioh/shms_delta_corr;
+	  //	}
 	  
-	  // I need to add the charge error and radiative dummy area (ELOG 522)
-	  //error is .15% for d/h and d. .3% for h 
-	  //Will get added to errd for d/h
+	  // Add the charge error and radiative dummy area (ELOG 522)
+	  // error is .15% for d/h and d. .3% for h 
+	  // Will get added to errd for d/h
+	  // NEed to think if this is correct for d, ok for d/h
 	  if(cs==1 || cs== 6){   
 	    if(target=="r"){
 	      errh=sqrt(errh*errh+qh_err*qh_err);
@@ -279,35 +282,36 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 	  Double_t xb=q2/2./mp/nu;
 	  Double_t w2= mp*mp + 2.*mp*nu-q2;
 	  if(abs(w2-w2d)>.001&&cs>=5){	  
+	    cout << "**** ERROR ***, w2 calc check!! ";
 	    cout <<"deltah, ep, w2, w2d, :"<<deltah<<"\t";
 	    cout<<ep << "\t" << w2 <<"\t"<< w2d <<endl;
 	  }
 
 	  bool goodW = false;
-
-	  //	  if(cs==5&&ratioh!=0)goodW = w2cut(spec,angle,mom,w2,binWidth,fm);//deltah=w2
-	  //	  cout << "good 1:  "<<goodW <<endl;
 	  if(cs>=5&&ratioh!=0)goodW = w2cut(spec,angle,mom,w2);//deltah=w2
+
+	  //  See how many points we are losing with w2 cut
 	  if(!otherSide && !goodW && ratioh!=0)leftPts++;
 	  if(goodW && ratioh!=0 )otherSide=true;
 	  if(otherSide && !goodW && ratioh!=0 )rightPts++;
-	  //	  goodW=true;
-	  //	  cout << "good 2:  "<<goodW <<endl;
+
+	  //  get model born xsec
 	  modeld=grd->Interpolate(w2,thetac);  //<<"\t"<<
 	  modelh=grh->Interpolate(w2,thetac);  //<<"\t"<<
+
 	  if(ratioh!=0 && ratiod!=0 && modelh!=0 && modeld!=0)
 	    {
 	      float cxh=(ratioh*modelh);
 	      float cxd=(ratiod*modeld);
 	      double sys_y=0.58;
 	      if(angle=="21"){ 
-		sys_y=.53;
+		sys_y=.56;
+		//		sys_y=.93;
 		//	   	   if(spec=="shms")sys_y=.525;
 		//	   if(spec=="hms")sys_y=.56;
 	      }
 	      if((cs==2 || (cs==7&&goodW)))//error band
 		{
-		  //		  if(xb<0.4)ofile << kin << "\t" << ep << "\t" << q2 << endl;
 		  if(target=="h")cx.push_back(0.);
 		  if(target=="d")cx.push_back(0.);
 		  if(target=="r")cx.push_back(sys_y);
@@ -327,18 +331,10 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 		    lte=abs(hlte_d->GetBinContent(getBin)-hlte_h->GetBinContent(getBin));
 		    boil_err=sqrt(pow(hboil_d->GetBinContent(getBin),2)+pow(hboil_h->GetBinContent(getBin),2) );
 		  }		  
-
-		  //		  cout << "D/H Live time error is " << lte << endl;
-		  //		  cout << "LH2 Live time error is " << hlte_h->GetBinContent(getBin) << endl;
-		  //		  cout << "LD2 Live time error is " << hlte_d->GetBinContent(getBin) << endl;
-		  //		  cout << "Do I get here?"<<endl;
-		  modErr=abs(gmodDep->Eval(w2)/100.);
-		    //		    cout << "ModErr: "<<modErr<<endl;
-		    int bin=i;
-		    //		    if(cs>=5)bin=hkinErr->FindBin(w2);
-		    //		    cout << "In extractCS. bin, w2 "<<bin<<",  "<<w2<<endl;
-		    val=getGlobalError(grd, grh, ep, w2, thetac, hsec, deltah, spec, angle, target, mom, xb, g_rad, hkinErr, bin, lte, charge_err, boil_err, modErr);  
-		    //		    cout <<"val from get Global Error : "<< val<<endl;
+		  if(angle!="59")modErr=abs(gmodDep->Eval(w2)/100.);
+		  int bin=i;
+		  //		    if(cs>=5)bin=hkinErr->FindBin(w2);
+		  val=getGlobalError(grd, grh, ep, w2, thetac, hsec, deltah, spec, angle, target, mom, xb, g_rad, hkinErr, bin, lte, charge_err, boil_err, modErr);  
 		  if(target=="h")cxe.push_back(val*cxh);
 		  if(target=="d")cxe.push_back(val*cxd);
 		  if(target=="r")cxe.push_back(val*cxd/cxh/2);		  
@@ -351,12 +347,6 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 
 	      if(cs==1 || cs==3 || (cs==8 && goodW) || (cs==6&&goodW))//cross section
 		{
-		  //		  cout << "good 3:  "<<goodW <<endl;	  
-		  //		  if(cs==1 || cs == 6){
-		  //		    if(target=="h")errh=sqrt(errh*errh+modErr*modErr);
-		  //		    if(target=="d")errh=sqrt(errd*errd+modErr*modErr);
-		  //		    if(target=="r")errh=sqrt(errh*errh+modErr*modErr);
-		  //		  }
 
 		  if(target=="h")cx.push_back(cxh);
 		  if(target=="d")cx.push_back(cxd);
@@ -369,6 +359,7 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 		  if(xaxis=="xb")eprime.push_back(xb);
 		  if(xaxis=="w2")eprime.push_back(w2);
 		  if(xaxis=="ep")eprime.push_back(ep);
+		  cout << ".............................xb, d/h: "<<xb<<"\t"<<cxd/cxh/2<<endl;
 		}
 	      if(cs==0 || (cs==5&& goodW))//data/model
 		{
@@ -377,7 +368,6 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
 		  wmax=w2;
 
 		  if(target=="h")cx.push_back(ratioh);
-		  //		  if(i==151)cout<<"Pushing back 151: "<<ratioh<<endl;
 		  if(target=="d")cx.push_back(ratiod);
 		  if(target=="r")cx.push_back(ratiod/ratioh);
 		  if(target=="h")cxe.push_back(errh);
@@ -407,9 +397,14 @@ TGraphErrors* extractCS(string spec="shms", string target="h", string angle="21"
       //      cout<<cx.at(i)<<"\t +/-";
       //      cout<<cxe.at(i)<<endl;
     }
-  //  ofile.close();
+  //    ofile.close();
   //  ofile2.close();
   TGraphErrors *gcx=new TGraphErrors(pts,&eprime[0],&cx[0],0,&cxe[0]);
-  return gcx;
+  TGraphAsymmErrors* gcx_glob = new TGraphAsymmErrors(pts,&eprime[0],&cx[0],0,0,0,&cxe[0]);
+  //TGraphAsymmErrors* gcx;
+
+  //  if(cs==2||cs==7){
+  if(cs==2||cs==7){return (TGraphErrors*)gcx_glob;}
+  else{return gcx;}
 }
 
